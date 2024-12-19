08c66cfb7d3b5f2eebd6c98b9301766da5c94780
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-19 22:44:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,22 +5,38 @@ from sshkeyboard import listen_keyboard
 led = LED(21)
 led.blink()
 
-servo = AngularServo(19, min_angle=-90, max_angle=90)
+servo1 = AngularServo(19, min_angle=-90, max_angle=90)
+servo2 = AngularServo(20, min_angle=-90, max_angle=90)
 
 def listen(key):
-    if servo.angle != 90:
+    if servo1.angle != 90:
         if key == "w":
-            print("w")
-            servo.angle += 10
-            print(servo.angle)
+            print("up")
+            servo1.angle += 10
+            print(servo1.angle)
     else:
         pass
 
-    if servo.angle != -90:
+    if servo1.angle != -90:
         if key == "s":
-            print("S")
-            servo.angle -= 10
-            print(servo.angle)
+            print("down")
+            servo1.angle -= 10
+            print(servo1.angle)
+
+def listen(key):
+    if servo2.angle != 90:
+        if key == "d":
+            print("right")
+            servo2.angle += 10
+            print(servo2.angle)
+    else:
+        pass
+
+    if servo2.angle != -90:
+        if key == "s":
+            print("left")
+            servo2.angle -= 10
+            print(servo2.angle)
 
     
 listen_keyboard(listen)
