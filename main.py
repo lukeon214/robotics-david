81a6915bf5c2e0325a8bba6859ea89d86948a6fa
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-19 22:00:34 +0100

    v4u1

--- main.py
+++ main.py
@@ -7,19 +7,13 @@ led.blink()
 
 servo = AngularServo(19, min_angle=-90, max_angle=90)
 
-servo_position = 0
 
 try:
     while True:
         if keyboard.is_pressed('w'):
-            if servo_position < 90:
-                servo.angle = servo_position + 1
-                servo_position += 1
+            servo.angle += 1
         elif keyboard.is_pressed('s'):
-            if servo_position > -90:
-                servo.angle = servo_position - 1
-                servo_position -= 1
+            servo.angle -= 1
 
-        time.sleep(0.05)
 except KeyboardInterrupt:
     pass
