95afdce477c2a35dfbbff3820489d4412edc241b
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-19 22:07:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 from gpiozero import AngularServo, LED
 import keyboard
-import time
+from sshkeyboard import listen_keyboard
 
 led = LED(21)
 led.blink()
 
 servo = AngularServo(19, min_angle=-90, max_angle=90)
 
-
-try:
-    while True:
-        if keyboard.is_pressed('w'):
-            print("w")
-            servo.angle += 1
-        elif keyboard.is_pressed('s'):
-            print("s")
-            servo.angle -= 1
-        print(servo.angle)
-except KeyboardInterrupt:
-    pass
+def listen(key):
+    if key == "w":
+        print("w")
+        servo.angle += 1
+    
+    elif key=="s":
+        print("s")
+        servo.angle -= 1
+    print(servo.angle)
+    
+listen_keyboard(listen)
