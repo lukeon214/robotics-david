aafc36f778afa3b815182209d2b7d41c138adeb3
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-19 21:08:27 +0100

    test2

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from time import sleep
 servo = AngularServo(19, min_angle=-90, max_angle=90)
 
 while True:
-    servo.angle = 360
+    servo.angle = 90
     sleep(3)
-    servo.angle = - 360
+    servo.angle = 90
     sleep(3)
