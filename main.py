2fb9a3b60510416ccc599402bdc98eef09c31919
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-19 23:08:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ led = LED(21)
 led.blink()
 
 servo1 = AngularServo(19, min_angle=-90, max_angle=90)
-servo2 = Motor(forward=4, backward=14)
 
 def listen(key):
     if servo1.angle != 90:
         if key == "w":
             print("up")
-            servo1.angle += 90
+            servo1.angle += 10
             print(servo1.angle)
     else:
         pass
@@ -19,17 +18,7 @@ def listen(key):
     if servo1.angle != -90:
         if key == "s":
             print("down")
-            servo1.angle -= 90
+            servo1.angle -= 10
             print(servo1.angle)
-
-def listen2(key):
-    if key == "d":
-        print("right")
-        servo2.forward()
-
-    if key == "a":
-        print("left")
-        servo2.backward()
-
     
-listen_keyboard(listen, listen2)
+listen_keyboard(listen)
