44dcc4658e15a6affaa451a2a7ad0d9378046a78
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-19 21:37:28 +0100

    v4

--- main.py
+++ main.py
@@ -1,36 +1,32 @@
 from gpiozero import AngularServo
 from gpiozero import LED
-from time import sleep
+from pynput import keyboard
 
 led = LED(21)
 led.blink()
 
-servo = AngularServo(19, min_angle=-90, max_angle=180)
+servo = AngularServo(19, min_angle=-90, max_angle=90)
 
-while True:
+servo_position = 0
+
+def on_press(key):
+    global servo_position
     try:
-        print("1. -90")
-        print("2. -45")
-        print("3. 0")
-        print("4. 45")
-        print("5. 90")
+        if key.char.lower() == 'w':
+            if servo_position < 90:
+                servo.angle = servo_position + 1
+                servo_position += 1
+        elif key.char.lower() == 's':
+            if servo_position > -90:
+                servo.angle = servo_position - 1
+                servo_position -= 1
 
-        nigg = int(input("Input: "))
+    except AttributeError:
+        pass
 
-        if nigg == 1:
-            servo.angle = -90
-            sleep(0)
-        elif nigg == 2:
-            servo.angle = -45
-            sleep(0)
-        elif nigg == 3:
-            servo.angle = 0
-            sleep(0)
-        elif nigg == 4:
-            servo.angle = 45
-            sleep(0)
-        elif nigg == 5:
-            servo.angle = 90
-            sleep(0)
-    except ValueError:
-        print("nigga")
+def on_release(key):
+    if key == keyboard.Key.esc:
+        return False
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
