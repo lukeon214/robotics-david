cbd04a5f524d0a1c354bfbc68c192b60d8440fb2
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-19 21:04:29 +0100

    servo

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from gpiozero import LED
+from gpiozero import AngularServo
 from time import sleep
-from signal import pause
 
-red = LED(21)
+servo = AngularServo(17, min_angle=-90, max_angle=90)
 
-red.blink()
-pause()
+while True:
+    servo.angle = -90
+    sleep(2)
+    servo.angle = -45
+    sleep(2)
+    servo.angle = 0
+    sleep(2)
+    servo.angle = 45
+    sleep(2)
+    servo.angle = 90
+    sleep(2)
