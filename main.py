ccde7c60499c9e059056f3b400293a0f2f8ada0b
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-19 21:06:00 +0100

    pin

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from gpiozero import AngularServo
 from time import sleep
 
-servo = AngularServo(17, min_angle=-90, max_angle=90)
+servo = AngularServo(19, min_angle=-90, max_angle=90)
 
 while True:
     servo.angle = -90
