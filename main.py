0e4db53d3bf3eee2d15ae78f348be8dbf78d47f7
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-19 22:13:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,15 @@ led.blink()
 servo = AngularServo(19, min_angle=-90, max_angle=90)
 
 def listen(key):
-    if key == "w":
-        print("w")
-        servo.angle += 5
+    if servo.angle <= 90:
+        if key == "w":
+            print("w")
+            servo.angle += 5
     
-    elif key=="s":
-        print("s")
-        servo.angle -= 5
-    print(servo.angle)
+    if servo.angle <= -90:
+        if key=="s":
+            print("s")
+            servo.angle -= 5
+            print(servo.angle)
     
 listen_keyboard(listen)
