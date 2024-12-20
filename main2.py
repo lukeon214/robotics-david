a38ee939b9ea2fa63e7f2fdabea3c5df940d29f0
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-20 19:11:23 +0100

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,16 +1,18 @@
 import pigpio
 import time
 
+pi = pigpio.pi()
+
 LED_PIN = 8
 pi.set_mode(LED_PIN, pigpio.OUTPUT)
+
 
 def blink_led():
     pi.write(LED_PIN, 1)
     time.sleep(0.01)
     pi.write(LED_PIN, 0)
     time.sleep(0.01) 
-
-pi = pigpio.pi()
+    
 pi.set_PWM_frequency(20,200)
 
 def map(x:float,in_min:float,in_max:float,out_min:float,out_max:float)-> float:
