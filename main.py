39dbc1ff43fc1773f7f55c2dc2031adfd9d5a5cc
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-20 18:17:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 from gpiozero import AngularServo
 from gpiozero.pins.pigpio import PiGPIOFactory
 from sshkeyboard import listen_keyboard
+from time import sleep
 
 factory = PiGPIOFactory()
 servo = AngularServo(20, min_angle=-90, max_angle=90, pin_factory=factory)
 
 def listen1(key):
-    if key == "w":
-        print("up")
-        servo.angle = min(90, servo.angle + 10)
+    servo.angle = min(90, servo.angle + 90)
+    sleep(5)
+    while servo.angle != -90:
+        servo.angle = max(-90, servo.angle - 1)
         print(servo.angle)
-
-    elif key == "s":
-        print("down")
-        servo.angle = max(-90, servo.angle - 10)
-        print(servo.angle)
+    else:
+        print("END!")
     
 print("Enter function")
 print("1. loop")
