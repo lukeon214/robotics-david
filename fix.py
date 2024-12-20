975906c4483faa52fbd9477c84d251fac9019943
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-20 15:33:51 +0100

    Create fix.py

---
+++ fix.py
@@ -0,0 +1,22 @@
+from gpiozero import AngularServo, LED
+from sshkeyboard import listen_keyboard
+
+
+servo1 = AngularServo(19, min_angle=-90, max_angle=90)
+
+def listen1(key):
+    if servo1.angle != 90:
+        if key == "w":
+            print("check 1. up")
+            servo1.angle += 10
+            print(servo1.angle)
+    else:
+        pass
+
+    if servo1.angle != -90:
+        if key == "s":
+            print("check 1. down")
+            servo1.angle -= 10
+            print(servo1.angle)
+
+listen_keyboard(listen1)
