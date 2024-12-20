f7ee65ac49a6a4235fd1641d8b229360eea7fde1
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-20 18:44:46 +0100

    Update main2.py

--- main2.py
+++ main2.py
@@ -8,14 +8,14 @@ def map(x:float,in_min:float,in_max:float,out_min:float,out_max:float)-> float:
 
 def setAngle(angle:float):
     pi.set_servo_pulsewidth(20,map(angle,-90,90,1000,1900))
-    
+
 while 1:
     for i in range(-90,90):
     
         setAngle(float(i))
         time.sleep(0.01)
 
-    for i in range(-90,90)[::-1]:
+    for i in range(90,-90):
     
         setAngle(float(i))
         time.sleep(0.01)
