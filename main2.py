295136eb4ed4e8de95ce6054a4b41cdc9ee797b7
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-20 18:27:25 +0100

    Update main2.py

--- main2.py
+++ main2.py
@@ -4,5 +4,8 @@ pi = pigpio.pi()
 
 def map(x:float,in_min:float,in_max:float,out_min:float,out_max:float)-> float:
         return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+def setAngle(angle:float):
+    pi.set_servo_pulsewidth(20,map(angle,-90,90,1000,1900))  
 while 1:
-    pi.set_servo_pulsewidth(20,map(float(input()),-90,90,1000,1900))
+    setAngle(float(input()))
