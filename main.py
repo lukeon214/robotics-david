69c2959fcabd72ef1e95919cac8e4a1c0b8e3a50
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-20 16:51:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ def listen1(key):
     if servo.angle != 90:
         if key == "w":
             print("up")
-            servo.angle += 1
+            servo.angle += 10
             print(servo.angle)
 
     if servo.angle != -90:
         if key == "s":
             print("down")
-            servo.angle -= 1
+            servo.angle -= 10
             print(servo.angle)
     
 listen_keyboard(listen1)
