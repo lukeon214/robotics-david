e32251eabf055051310deaa41cc49184ca1c8a08
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-20 16:41:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,53 +2,19 @@ from gpiozero import AngularServo
 from sshkeyboard import listen_keyboard
 
 
-servo1 = AngularServo(19, min_angle=-90, max_angle=90)
-servo2 = AngularServo(20, min_angle=-90, max_angle=90)
-servo3 = AngularServo(21, min_angle=-90, max_angle=90)
-
-servo1.angle = 0
+servo = AngularServo(19, min_angle=-90, max_angle=90)
 
 def listen1(key):
-    if servo1.angle != 90:
+    if servo.angle != 90:
         if key == "w":
-            print("check 1. up")
-            servo1.angle += 1
-            print(servo1.angle)
+            print("up")
+            servo.angle += 1
+            print(servo.angle)
 
-    if servo1.angle != -90:
+    if servo.angle != -90:
         if key == "s":
-            print("check 1. down")
-            servo1.angle -= 1
-            print(servo1.angle)
-
-def listen2(key):
-    if servo2.angle != 90:
-        if key == "e":
-            print("check 2. up")
-            servo2.angle += 10
-            print(servo2.angle)
-    else:
-        pass
-
-    if servo2.angle != -90:
-        if key == "d":
-            print("check 2. down")
-            servo2.angle -= 10
-            print(servo2.angle)
-
-def listen3(key):
-    if servo3.angle != 90:
-        if key == "q":
-            print("open")
-            servo3.angle += 45
-            print(servo3.angle)
-    else:
-        pass
-
-    if servo3.angle != -90:
-        if key == "a":
-            print("close")
-            servo3.angle -= 45
-            print(servo3.angle)
+            print("down")
+            servo.angle -= 1
+            print(servo.angle)
     
-listen_keyboard(listen1, listen2, listen3)
+listen_keyboard(listen1)
