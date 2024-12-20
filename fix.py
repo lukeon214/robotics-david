cb5e85b60a474156cc641fc900460f587f7ec734
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-20 15:44:27 +0100

    Update fix.py

--- fix.py
+++ fix.py
@@ -1,13 +1,15 @@
-from gpiozero import AngularServo, LED
+from gpiozero import AngularServo, LED, Motor
 from sshkeyboard import listen_keyboard
 
-servo1 = AngularServo(19, min_angle=-90, max_angle=90)
-servo3 = AngularServo(21, min_angle=-90, max_angle=90)
+led = LED(21)
+led.blink()
 
-def listen1(key):
+servo1 = AngularServo(19, min_angle=-90, max_angle=90)
+
+def listen(key):
     if servo1.angle != 90:
         if key == "w":
-            print("check 1. up")
+            print("up")
             servo1.angle += 10
             print(servo1.angle)
     else:
@@ -15,23 +17,8 @@ def listen1(key):
 
     if servo1.angle != -90:
         if key == "s":
-            print("check 1. down")
+            print("down")
             servo1.angle -= 10
             print(servo1.angle)
-
-def listen3(key):
-    if servo3.angle != 90:
-        if key == "q":
-            print("open")
-            servo3.angle += 45
-            print(servo3.angle)
-    else:
-        pass
-
-    if servo3.angle != -90:
-        if key == "a":
-            print("close")
-            servo3.angle -= 45
-            print(servo3.angle)
-
-listen_keyboard(listen1, listen3)
+    
+listen_keyboard(listen)
