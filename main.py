0d39890c1d6898ccc2e6508145cfd2378288b8f7
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-20 17:27:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from gpiozero import AngularServo
-from sshkeyboard import listen_keyboard
+from gpiozero.support.pins.pigpio import PigpioFactory
 
+factory = PigpioFactory()
+AngularServo = factory-angular-servo-cls  # Register the pin factory with gpiozero
 
 servo = AngularServo(20, min_angle=-90, max_angle=90)
 
