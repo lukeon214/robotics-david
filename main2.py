eebc22d5e9a2caf4ee1414e0b5531c0a651f7a01
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-20 18:48:22 +0100

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,14 +1,14 @@
 import pigpio
 import time
 pi = pigpio.pi()
-
+pi.set_PWM_frequency(20,200)
 
 def map(x:float,in_min:float,in_max:float,out_min:float,out_max:float)-> float:
         return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 def setAngle(angle:float):
     pi.set_servo_pulsewidth(20,map(angle,-90,90,1000,1900))
-    pi.set_PWM_frequency(20,200)    
+        
 while 1:
     for i in range(-90,90):
     
