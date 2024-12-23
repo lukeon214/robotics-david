5724e26edc269786ca2790980d581a8e785d28a7
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-23 22:23:04 +0100

    Create center.py

---
+++ center.py
@@ -0,0 +1,21 @@
+import pigpio
+import time
+
+pi = pigpio.pi()
+
+SERVO_PIN = 20
+pi.set_PWM_frequency(SERVO_PIN, 200)
+
+def map(x: float, in_min: float, in_max: float, out_min: float, out_max: float) -> float:
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+
+def setAngleLoop():
+    while True:
+        setAngle(0)
+
+def setAngle(angle: float):
+    pulse_width = map(angle, -90, 90, 1000, 1900)
+    pi.set_servo_pulsewidth(SERVO_PIN, pulse_width)
+
+setAngle()
