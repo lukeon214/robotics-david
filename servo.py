3e7d111cb6834ccb14842e550e94a2b5512877b7
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-30 22:31:28 +0100

    Update servo.py

--- servo.py
+++ servo.py
@@ -4,8 +4,10 @@ from sshkeyboard import listen_keyboard
 
 kit = ServoKit(channels=16)
 
-kit.servo[1].angle = 90
-time.sleep(5)
 kit.servo[1].angle = 0
 time.sleep(5)
 kit.servo[1].angle = 45
+time.sleep(5)
+kit.servo[1].angle = 90
+time.sleep(5)
+kit.servo[1].angle = 0
