b4ac09023370524ef5a42738ff40f8dad30f3d1e
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-30 17:18:28 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ from adafruit_servokit import ServoKit
 import time
 kit = ServoKit(channels=16)
 
-kit.continuous_servo[0].throttle = 10
+kit.continuous_servo[0].throttle = 0.9
 time.sleep(5)
-kit.continuous_servo[0].throttle = 0
+kit.continuous_servo[0].throttle = 0.0
