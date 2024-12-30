785d3019e81e8ef43c21d7f5bfeb9819dbe63c1b
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-30 17:33:09 +0100

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ kit = ServoKit(channels=16)
 
 def on_key_press(key):
     if key == "a":
-        kit.continuous_servo[0].throttle = 0.5
+        kit.continuous_servo[0].throttle = 0.1
 
 def on_key_release(key):
     if key == "a":
