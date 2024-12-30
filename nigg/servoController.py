949f6224a8396b90d89ca2acc6deb0cde47bd62d
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-30 19:52:40 +0100

    Create servoController.py

---
+++ nigg/servoController.py
@@ -0,0 +1,22 @@
+from adafruit_servokit import ServoKit
+import time
+
+class RoboticArm:
+    def __init__(self, channels=16):
+        self.kit = ServoKit(channels=channels)
+
+    def set_servo_angle(self, servo_number, angle):
+        if 0 <= servo_number < len(self.kit.servo):
+            self.kit.servo[servo_number].angle = angle
+        else:
+            raise ValueError("Stupid ass nigger!")
+
+
+arm = RoboticArm()
+
+try:
+    arm.set_servo_angle(0, 45)
+    arm.set_servo_angle(1, 90)
+    arm.set_servo_angle(2, 20)
+except Exception as e:
+    print(f"Error: {e}")
