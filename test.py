26189bc1f22db0e6a2162ab6447d663de3c006ac
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-30 17:31:59 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,12 +4,15 @@ from sshkeyboard import listen_keyboard
 
 kit = ServoKit(channels=16)
 
-def listen(key):
+def on_key_press(key):
     if key == "a":
         kit.continuous_servo[0].throttle = 1.0
+
+def on_key_release(key):
+    if key == "a":
         kit.continuous_servo[0].throttle = 0.0
 
-    
-
-
-listen_keyboard(listen)
+listen_keyboard(
+    on_press=on_key_press,
+    on_release=on_key_release
+)
