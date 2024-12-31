ebfaa6968e40b255b156c86b60aa5ab19e8d6c72
Lukáš Hájek <lukas.hajek@zsmnichovice.cz>
2024-12-31 19:36:56 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+from adafruit_servokit import ServoKit
+import time
+import threading
+
+kit = ServoKit(channels=16)
+
+kit.servo[1].angle = 0
+time.sleep(2)
+kit.servo[1].angle = 180
+time.sleep(2)
+kit.servo[1].angle = 0
