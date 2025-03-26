47a8c6c1e87a303bf9e2d6ccd2898dbde732d084
lukeon <163727888+lukeon214@users.noreply.github.com>
2025-03-26 15:49:32 +0100

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,29 +1,15 @@
 from adafruit_servokit import ServoKit
 import time
-from sshkeyboard import listen_keyboard
 
 kit = ServoKit(channels=16)
 
-kit.servo[1].angle = 0
-time.sleep(5)
-kit.servo[1].angle = 45
-time.sleep(5)
-kit.servo[1].angle = 90
-time.sleep(5)
-kit.servo[1].angle = 0
+servos = [0, 1, 2, 3, 4]
+angles = [0, 30, 45, 60, 90, 120, 150, 180, 0]
 
-kit.servo[2].angle = 0
-time.sleep(5)
-kit.servo[2].angle = 45
-time.sleep(5)
-kit.servo[2].angle = 90
-time.sleep(5)
-kit.servo[2].angle = 0
+def move_servos():
+    for angle in angles:
+        for servo in servos:
+            kit.servo[servo].angle = angle
+        time.sleep(2)
 
-kit.servo[3].angle = 0
-time.sleep(5)
-kit.servo[3].angle = 45
-time.sleep(5)
-kit.servo[3].angle = 90
-time.sleep(5)
-kit.servo[3].angle = 0
+move_servos()
