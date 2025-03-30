5a2395cfa3ce4e9225cb464f0bb7cde8e76850da
lukeon <163727888+lukeon214@users.noreply.github.com>
2025-03-30 23:03:52 +0200

    Update servo.py

--- servo.py
+++ servo.py
@@ -3,7 +3,7 @@ import time
 
 kit = ServoKit(channels=16)
 
-servos = [0, 1, 2, 3, 4, 5]
+servos = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 angles = [0, 30, 45, 60, 90, 120, 150, 180, 0]
  
 def move_servos():
