3e12463879ae02fade22075aade7c16000dee7b9
lukeon <163727888+lukeon214@users.noreply.github.com>
2025-03-30 20:34:04 +0200

    Update servo.py

--- servo.py
+++ servo.py
@@ -4,13 +4,13 @@ import time
 kit = ServoKit(channels=16)
 
 servos = [0, 1, 2, 3, 4, 5]
-angles = [0, 30, 45, 60, 90, 120, 150, 180, 0]
+angles = [0, 90, 180]
 
 def move_servos():
     for angle in angles:
         for servo in servos:
             kit.servo[servo].angle = angle
-        time.sleep(0.05)
+        time.sleep(2)
 
 while True:
     move_servos()
