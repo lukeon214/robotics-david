5a11df6dbea3551d5d84c10f7769028403e1a986
lukeon <163727888+lukeon214@users.noreply.github.com>
2025-04-06 22:17:41 +0200

    Update hexapod.py

--- hexapod/hexapod.py
+++ hexapod/hexapod.py
@@ -5,15 +5,12 @@ kit = ServoKit(channels=16)
 
 delay = 0.02
 
-for i in range(16):
-    print(f"Moving servo on channel {i}")
-
-    for angle in range(0, 181, 1):
+for angle in range(0, 181, 1):
+    for i in range(16):
         kit.servo[i].angle = angle
-        time.sleep(delay)
+    time.sleep(delay)
 
-    for angle in range(180, -1, -1):
+for angle in range(180, -1, -1):
+    for i in range(16):
         kit.servo[i].angle = angle
-        time.sleep(delay)
-
-    print(f"Done with servo {i}\n")
+    time.sleep(delay)
