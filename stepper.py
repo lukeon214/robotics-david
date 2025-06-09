e5dd25928ea2db648f4bacbaf4c5ada5d523eb86
lukeon <lukeon.hajek@gmail.com>
2025-06-09 18:37:42 +0200

    Update stepper.py

--- stepper.py
+++ stepper.py
@@ -3,4 +3,9 @@ import RPi.GPIO as GPIO
 DIR_PIN = 18   
 STEP_PIN = 17  
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(DIR_PIN, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(STEP_PIN, GPIO.OUT, initial=GPIO.LOW)
+
 GPIO.output(STEP_PIN, GPIO.HIGH)
