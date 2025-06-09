bb5f4f0b7743a024293a48adc46298d8d2daf919
lukeon <lukeon.hajek@gmail.com>
2025-06-09 18:44:10 +0200

    Update stepper.py

--- stepper.py
+++ stepper.py
@@ -8,10 +8,19 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(DIR_PIN, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(STEP_PIN, GPIO.OUT, initial=GPIO.LOW)
-direction = False
 
-while True:
+def motor(direction=True):
+    GPIO.output(DIR_PIN, GPIO.HIGH if direction else GPIO.LOW)
     GPIO.output(STEP_PIN, GPIO.HIGH)
     time.sleep(0.001)
     GPIO.output(STEP_PIN, GPIO.LOW)
     time.sleep(0.001)
+    
+try:
+    motor(direction=False)
+
+finally:
+    GPIO.output(STEP_PIN, GPIO.LOW)
+    GPIO.output(DIR_PIN, GPIO.LOW)
+    
+    GPIO.cleanup()
