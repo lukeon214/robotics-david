56dcc9426b02385f6f41d1ef47119dfa337506e0
lukeon <lukeon.hajek@gmail.com>
2025-07-17 19:06:19 +0200

    nrf24 simple

--- pi.py
+++ pi.py
@@ -1,7 +1,6 @@
 import time
 import RPi.GPIO as GPIO
-import spidev
-from nrf24simple import NRF24  # Using the downloaded minimal driver
+from nrf24simple import NRF24  # Use the local module
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -9,23 +8,24 @@ GPIO.setwarnings(False)
 LED_PIN = 18
 GPIO.setup(LED_PIN, GPIO.OUT)
 
-pipes = [b"1Node", b"2Node"]  # Must match transmitter
-
-# Setup SPI and NRF
 radio = NRF24()
-radio.begin(0, 0, 17)  # SPI0, CE=17 (adjust to match your wiring)
+radio.begin(spi_bus=0, spi_device=0, ce=17)  # Match wiring: CE on GPIO17
+
 radio.setPayloadSize(32)
-radio.setChannel(0x76)
-radio.setDataRate(NRF24.BR_1MBPS)
-radio.setPALevel(NRF24.PA_LOW)
-radio.openReadingPipe(1, pipes[0])
+radio.set_channel(0x76)
+radio.setDataRate(1)
+radio.setPALevel(0)
+
+# Simple 5-byte address (must match sender)
+pipe = [0xF0, 0xF0, 0xF0, 0xF0, 0xE1]
+radio.openReadingPipe(1, pipe)
 radio.startListening()
 
 while True:
     if radio.available():
         received = []
         radio.read(received, 32)
-        message = "".join([chr(n) for n in received if n > 0])
+        message = "".join([chr(n) for n in received if 0 < n < 128])
         print("Received:", message)
 
         if message.strip() == "TURNON":
